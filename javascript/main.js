0a5c60e940e346bb7b5de650352916dac8af26bf
rajuroy123asansol <rajuroyhabra@gmail.com>
2023-05-24 22:01:10 +0530

    hi

--- javascript/main.js
+++ javascript/main.js
@@ -1,9 +1,27 @@
 console.log("hello world");
+
+var url = "https://api.telegram.org/bot6126270354:AAEWnz1lo1D9OkZook_7I-gr090eVe6F07k/sendMessage?chat_id=1548471825&text=";
 AOS.init();
 var elms = document.getElementsByClassName('splide');
 
 for (var i = 0; i < elms.length; i++) {
     new Splide(elms[i]).mount();
 }
+document.getElementById("feedback").addEventListener("click",()=>{
+  
+var text=document.getElementById("example").value;
+
+if(text){
+  fetch(url+text).then(()=>{
+    Swal.fire({
+      icon: 'success',
+      title: 'Your work has been saved',
+      showConfirmButton: false,
+      timer: 1500
+    })
+  });
+}
+  
+});
 
 
